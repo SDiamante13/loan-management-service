9a5bd28ff85556cecc42be88216a310e80fdc067
Steven Diamante <stevendiamante13@gmail.com>
2025-05-22 10:49:40 -0400

    [s5] Render message for needs manual review on UI.

--- src/main/java/com/diamantetechcoaching/loanmanagement/ui/LoanApplicationFormController.java
+++ src/main/java/com/diamantetechcoaching/loanmanagement/ui/LoanApplicationFormController.java
@@ -86,6 +86,7 @@ public class LoanApplicationFormController {
                 
                 model.addAttribute("response", response);
                 model.addAttribute("approved", "Approved".equals(response.getStatus()));
+                model.addAttribute("status", response.getStatus());
             } else {
                 // Handle null response
                 model.addAttribute("error", "Unable to process your application at this time. Please try again later.");
