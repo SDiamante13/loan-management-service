b6ccc2e6415173aeb716d1efa16ef8ec9557ae52
Steven Diamante <stevendiamante13@gmail.com>
2025-07-13 22:52:38 -0400

    . t Add Approval and Mutation Testing.

--- src/test/java/com/diamantetechcoaching/loanmanagement/LoanApplicationServiceTest.java
+++ src/test/java/com/diamantetechcoaching/loanmanagement/LoanApplicationServiceTest.java
@@ -1,30 +1,62 @@
 package com.diamantetechcoaching.loanmanagement;
 
+import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest
 class LoanApplicationServiceTest {
 
-    @Autowired
-    LoanApplicationService loanApplicationService;
+    LoanApplicationService loanApplicationService = new LoanApplicationService();
 
     @Test
-    void processLoanApplication() {
-        LoanApplicationRequest request = new LoanApplicationRequest(
+    void rejectedLoans() {
+        LoanApplicationResponse lowCreditScore = loanApplicationService.processLoanApplication(new LoanApplicationRequest(
                 "first",
                 "last",
-                10000,
-                1000,
-                30000,
-                "123"
-        );
+                5_000,
+                1_500,
+                18_000,
+                "999"
+        ), 700, loanEntity -> {
+        });
+        LoanApplicationResponse highDebtToIncomeRatio = loanApplicationService.processLoanApplication(new LoanApplicationRequest(
+                "first",
+                "last",
+                5_000,
+                10_000,
+                18000,
+                "999"
+        ), 750, loanEntity -> {
+        });
+        LoanApplicationResponse loanAmountTooHigh = loanApplicationService.processLoanApplication(new LoanApplicationRequest(
+                "first",
+                "last",
+                5_000,
+                1_500,
+                200_000,
+                "999"
+        ), 750, loanEntity -> {
+        });
 
-        LoanApplicationResponse response = loanApplicationService.processLoanApplication(request, 750, entity1 -> {});
+        Approvals.verifyAll("Rejected Loans",
+                new LoanApplicationResponse[]{
+                        lowCreditScore,
+                        highDebtToIncomeRatio,
+                        loanAmountTooHigh
+                });
+    }
 
-        assertThat(response.getStatus()).isEqualTo("Approved");
+    @Test
+    void approvedLoans() {
+        LoanApplicationResponse approvedLoan = loanApplicationService.processLoanApplication(new LoanApplicationRequest(
+                "Bruce",
+                "Banner",
+                5_000,
+                1_500,
+                18_000,
+                "999"
+        ), 750, loanEntity -> {
+        });
+
+        Approvals.verify(approvedLoan);
     }
 }
