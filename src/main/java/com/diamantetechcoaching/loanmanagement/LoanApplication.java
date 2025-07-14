2ea38ea7e67f4dc563456c7aae830c5be1347f03
Steven Diamante <stevendiamante13@gmail.com>
2025-07-13 23:10:30 -0400

    ^ r Map domain to entity. Map domain to response.

--- src/main/java/com/diamantetechcoaching/loanmanagement/LoanApplication.java
+++ src/main/java/com/diamantetechcoaching/loanmanagement/LoanApplication.java
@@ -1,5 +1,10 @@
 package com.diamantetechcoaching.loanmanagement;
 
+import com.diamantetechcoaching.loanmanagement.entity.LoanEntity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public record LoanApplication(String firstName, String lastName, double monthlyIncome, double monthlyDebt,
                               double requestedAmount, String ssn, int creditScore) {
 
@@ -13,5 +18,34 @@ public record LoanApplication(String firstName, String lastName, double monthlyI
                 request.getSsn(),
                 creditScore
         );
+    }
+
+    LoanEntity toLoanEntity(int creditScore, String loanStatus) {
+        LoanEntity entity = new LoanEntity();
+        entity.setFirstName(firstName());
+        entity.setLastName(lastName());
+        entity.setCreditScore(creditScore);
+        entity.setMonthlyIncome(BigDecimal.valueOf(monthlyIncome()));
+        entity.setMonthlyDebt(BigDecimal.valueOf(monthlyDebt()));
+        entity.setRequestedAmount(BigDecimal.valueOf(requestedAmount()));
+        entity.setDebtToIncomeRatio(BigDecimal.valueOf(calculateDebtToIncomeRatio()));
+        entity.setApplicationStatus(loanStatus);
+        entity.setSubmissionTimestamp(LocalDateTime.now());
+        return entity;
+    }
+
+    LoanApplicationResponse toLoanApplicationResponse(int creditScore, String loanStatus) {
+        return new LoanApplicationResponse(
+                loanStatus,
+                creditScore,
+                monthlyIncome(),
+                monthlyDebt(),
+                requestedAmount(),
+                calculateDebtToIncomeRatio()
+        );
+    }
+
+    double calculateDebtToIncomeRatio() {
+        return (monthlyDebt() / monthlyIncome()) * 100;
     }
 }
--- src/main/java/com/diamantetechcoaching/loanmanagement/LoanApplicationService.java
+++ src/main/java/com/diamantetechcoaching/loanmanagement/LoanApplicationService.java
@@ -7,8 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.function.Consumer;
 
 @Service
@@ -39,36 +37,13 @@ public class LoanApplicationService {
     LoanApplicationResponse processLoanApplication(LoanApplicationRequest request, int creditScore, Consumer<LoanEntity> saveAction) {
         LoanApplication loanApplication = LoanApplication.of(request, creditScore);
         String loanStatus = "Rejected";
-        if (creditScore >= 750 && calculateDebtToIncomeRatio(loanApplication) <= 35 && loanApplication.requestedAmount() <= loanApplication.monthlyIncome() * 4) {
+        if (creditScore >= 750 && loanApplication.calculateDebtToIncomeRatio() <= 35 && loanApplication.requestedAmount() <= loanApplication.monthlyIncome() * 4) {
             loanStatus = "Approved";
         }
 
-        LoanApplicationResponse response = new LoanApplicationResponse(
-                loanStatus,
-                creditScore,
-                loanApplication.monthlyIncome(),
-                loanApplication.monthlyDebt(),
-                loanApplication.requestedAmount(),
-                calculateDebtToIncomeRatio(loanApplication)
-        );
-
-        LoanEntity entity = new LoanEntity();
-        entity.setFirstName(loanApplication.firstName());
-        entity.setLastName(loanApplication.lastName());
-        entity.setCreditScore(creditScore);
-        entity.setMonthlyIncome(BigDecimal.valueOf(loanApplication.monthlyIncome()));
-        entity.setMonthlyDebt(BigDecimal.valueOf(loanApplication.monthlyDebt()));
-        entity.setRequestedAmount(BigDecimal.valueOf(loanApplication.requestedAmount()));
-        entity.setDebtToIncomeRatio(BigDecimal.valueOf(calculateDebtToIncomeRatio(loanApplication)));
-        entity.setApplicationStatus(loanStatus);
-        entity.setSubmissionTimestamp(LocalDateTime.now());
-
+        LoanEntity entity = loanApplication.toLoanEntity(creditScore, loanStatus);
         saveAction.accept(entity);
 
-        return response;
-    }
-
-    private static double calculateDebtToIncomeRatio(LoanApplication loanApplication) {
-        return (loanApplication.monthlyDebt() / loanApplication.monthlyIncome()) * 100;
+        return loanApplication.toLoanApplicationResponse(creditScore, loanStatus);
     }
 }
